61e7f5093c90678a9ec16f469dffcba69edd1ac2
Lachlan <lachlanatmc@gmail.com>
2025-12-15 12:42:39 +1100

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: "/our-timeline/",
+    base: "/our-timeline",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
